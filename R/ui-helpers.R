559d2181f4c502f9daf315c1ba5bc6354716d3b9
Cypress Frankenfeld <iamcypress@gmail.com>
2020-03-12 20:34:13 -0400

    Change regions to match the MIT ones

--- R/ui-helpers.R
+++ R/ui-helpers.R
@@ -9,29 +9,23 @@ NULL
 
 #' @rdname rgnlists
 #' @export
-africa.rgns <- c('Africa_Eastern', 'Africa_Northern', 'Africa_Southern',
-                 'Africa_Western', 'South Africa')
+africa.rgns <- c('AFR')
 
 #' @rdname rgnlists
 #' @export
-north.america.rgns <- c('USA', 'Canada')
+north.america.rgns <- c('Canada', 'USA')
 
 #' @rdname rgnlists
 #' @export
-lac.rgns <- c('Argentina', 'Brazil', 'Central America and Caribbean',
-              'Colombia','Mexico', 'South America_Northern',
-              'South America_Southern')
+lac.rgns <- c('Brazil', 'LAM', 'MES', 'Mexico', 'REA', 'ROE')
 
 #' @rdname rgnlists
 #' @export
-europe.rgns <- c('EU-12', 'EU-15', 'Europe_Eastern', 'Europe_Non_EU',
-                 'European Free Trade Association')
+europe.rgns <- c('EUR')
 
 #' @rdname rgnlists
 #' @export
-asiapac.rgns <- c('Australia_NZ', 'Central Asia', 'China', 'India', 'Indonesia',
-                  'Japan', 'Middle East', 'Pakistan', 'Russia', 'South Asia',
-                  'South Korea', 'Southeast Asia')
+asiapac.rgns <- c('ASI', 'IDZ', 'Australia_NZ', 'China', 'India', 'Japan', 'KOR', 'Russia')
 
 
 ## available color schemes
